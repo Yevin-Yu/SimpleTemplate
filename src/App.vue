d81ebe8bf18789a2f71919bb8298df0b009d5dec
Yevin <yuwb0521@outlook.com>
2025-12-08 22:15:24 +0800

    Init:V2.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,30 @@
-<template>
-  <div id="app">
-    <elementTablePlus :option="option"></elementTablePlus>
-  </div>
-</template>
-
-<script>
-
-// import elementTablePlus from "./package/element-table-plus.vue";
-export default {
-  name: "App",
-  // components: {
-
-  //   elementTablePlus,
-  // },
-  data() {
-    return {
-      option: {},
-    };
-  },
-  created() {
-    this.option = require("./package/data.json");
-  },
-};
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  margin: 100px;
-  background-color: pink;
-  border: 1px solid #eee;
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
