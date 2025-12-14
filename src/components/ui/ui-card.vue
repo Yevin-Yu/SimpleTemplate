adb6e97bf3b360c2fb1e8fb5da2edbde01680831
Yevin <yuwb0521@outlook.com>
2025-12-14 14:56:28 +0800

    v3.0.2-feat:ui-card

---
+++ src/components/ui/ui-card.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="ui-card">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="less">
+.ui-card {
+    border: 1px solid var(--border);
+    box-shadow: var(--shadow);
+}
+</style>
